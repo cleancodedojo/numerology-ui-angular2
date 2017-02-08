7060f7e637f74d4513669b3ca2f78398c9973c9d
raydecastro <captray108@gmail.com>
2017-02-08 04:19:31 -0800

    feature. added styling and made tests and e2e tests pass.

--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -6,6 +6,6 @@ export class NumerologyUiAngular2Page {
   }
 
   getParagraphText() {
-    return element(by.css('app-root h1')).getText();
+    return element(by.css('app-root h3')).getText();
   }
 }
