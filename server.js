e6f8ac4edac374897c50a885958c09bf30178d5c
raydecastro <captray108@gmail.com>
2017-02-08 03:30:28 -0800

    bugfix. removed line that is preventing load.

--- server.js
+++ server.js
@@ -13,10 +13,6 @@ const forceSSL = function() {
   }
 }
 
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
-});
-
 app.use(forceSSL());
 app.use(express.static(__dirname + '/dist'));
 app.listen(process.env.PORT || 8080);
